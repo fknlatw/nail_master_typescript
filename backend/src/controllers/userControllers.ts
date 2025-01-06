3ea551f1eb339bea9fe05ca64e9c4539f0a5af5f
fknlatw <argat2480@gmail.com>
2025-01-06 23:37:59 +0600

    added user routes

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -3,8 +3,9 @@ import jwt, { Secret } from 'jsonwebtoken';
 import pool from "../db";
 import bcrypt from 'bcryptjs';
 import "dotenv/config.js";
-import { User, UserWithoutId } from "../interfaces/Interfaces";
+import { User, UserWithoutId, UserWithoutPassword } from "../interfaces/interfaces";
 import { RowDataPacket } from "mysql2";
+import { ApplicableRefactorInfo } from "typescript";
 
 
 const generateRefreshToken = (user: User): string => {
@@ -29,41 +30,97 @@ const generateAccessToken = (user: User): string => {
 
 export const loginUser = async (req: Request, res: Response) => {
     const { userPassword, userName }: UserWithoutId = req.body;
-    res.status(200).json({message: "login"})
+
+    pool.query(
+        "SELECT * FROM users WHERE userName = ?", 
+        [userName], 
+        async (err, results: RowDataPacket[]) => {
+            if(err) return res.status(500)
+            .json({message: "Ошибка на сервере"});
+
+            if(results.length === 0) return res.status(400)
+            .json({message: "Пользователь с таким именем не существует"});
+
+            const user = results[0] as User;
+
+            const passwordMatch = await bcrypt.compare(userPassword, user.userPassword);
+
+            if(!passwordMatch) return res.status(400)
+            .json({message: "Неверный пароль"});
+
+            const accessToken = generateAccessToken(user);
+            const refreshToken = generateRefreshToken(user);
+
+            pool.query(
+                "INSERT INTO tokens (token, userId) VALUES (?, ?)", 
+                [refreshToken, user.userId], 
+                (err) => {
+                    if(err) return res.status(500)
+                    .json({message: "Ошибка на сервере"});
+
+                    res.cookie("refreshToken", refreshToken, {
+                        httpOnly: true, 
+                        secure: true, 
+                        maxAge: 7 * 24 * 60 * 60 * 1000
+                    });
+
+                    res.cookie("accessToken", accessToken, {
+                        httpOnly: true,
+                        secure: true,
+                        maxAge: 15 * 60 * 1000
+                    })
+                    res.status(200).json({message: "Выполнен вход"});
+                }
+            );
+        }
+    );
 }
 
 export const registerUser = async (req: Request, res: Response) => {
-    // const { userPassword, userName }: UserWithoutId = req.body;
+    const { userPassword, userName }: UserWithoutId = req.body;
 
-    // const hashedUserPassword: string = await bcrypt.hash(userPassword, 5);
+    const hashedUserPassword: string = await bcrypt.hash(userPassword, 5);
 
-    // pool.query(
-    //     "SELECT * FROM users WHERE userName = ?", 
-    //     [userName],
-    //     (err, results: RowDataPacket[]) => {
-    //         if(err) return res.status(500)
-    //         .json({message: "Ошибка на сервере"});
+    pool.query(
+        "SELECT * FROM users WHERE userName = ?", 
+        [userName],
+        (err, results: RowDataPacket[]) => {
+            if(err) return res.status(500)
+            .json({message: "Ошибка на сервере"});
             
-    //         if(results.length > 0) return res.status(400)
-    //         .json({message: "Пользователь с таким именем уже существует"});
+            if(results.length > 0) return res.status(400)
+            .json({message: "Пользователь с таким именем уже существует"});
 
-    //         // pool.query(
-    //         //     "INSERT INTO users (userName, userPassword) VALUES (?, ?)",
-    //         //     [userName, hashedUserPassword],
-    //         //     (err) => {
-    //         //         if(err) return res.status(500)
-    //         //         .json({message: "Ошибка на сервере"});
+            pool.query(
+                "INSERT INTO users (userName, userPassword) VALUES (?, ?)",
+                [userName, hashedUserPassword],
+                (err) => {
+                    if(err) return res.status(500)
+                    .json({message: "Ошибка на сервере"});
                     
-    //                 res.status(200).json({message: "Пользователь успешно зарегистрирован"});
-    //         //     }
-    //         // )
-            
-    //     }    
-    // )
-        
-        
-        
-        
+                    res.status(200).json({message: "Пользователь успешно зарегистрирован"});
+                }
+            );
+        }    
+    );
+}
+
+export const logoutUser = async (req: Request, res: Response | Record<string, any>) => {
+    const refreshToken: string = req.cookies?.refreshToken;
+    if(!refreshToken) return res.status(400).json({message: "Необходимо авторизоваться"});
+    res.clearCookie("refreshToken");
+    res.clearCookie("accessToken");
     
+    const decoded = jwt.decode(refreshToken, {}) as UserWithoutPassword | null;
     
+    pool.query(
+        "DELETE FROM tokens WHERE userId = ?", 
+        [decoded?.userId], 
+        (err) => {
+            if(err) return res.status(500)
+            .json({message: "Ошибка на сервере"});
+
+            res.status(200).json({message: "Выполнен выход"});
+        }
+    );
 }
